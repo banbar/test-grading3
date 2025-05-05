f6feb2917f83df606621143c84b572cb92d19d58
banbar <banbar@hacettepe.edu.tr>
2025-05-05 09:48:13 +0300

    second

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -13,29 +13,3 @@ def bubble_sort( theSeq ):
         return theSeq
     
         
-
-def selection_sort( theSeq ):
-    n = len( theSeq )
-    for i in range( n - 1 ):
-        smallNdx = i
-        for j in range( i + 1, n ):
-            if theSeq[j] < theSeq[smallNdx]:
-                smallNdx = j
-        if smallNdx != i:
-            tmp = theSeq[i]
-            theSeq[i] = theSeq[smallNdx]
-            theSeq[smallNdx] = tmp
-        print(theSeq)
-    return theSeq
-
-def insertion_sort( theSeq ):
-    n = len( theSeq )
-    for i in range( 1, n):
-        value = theSeq[i]
-        pos = i
-        while pos > 0 and value < theSeq[pos - 1]:
-            theSeq[pos] = theSeq[pos - 1]
-            pos -= 1
-        theSeq[pos] = value
-        print(theSeq)
-    return theSeq
